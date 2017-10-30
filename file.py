9775bb77c8ac7b44d052ca8b36c04da2e71c8150
View <xyc986327386@gmail.com>
2017-10-30 20:38:25 +0800

    v1.1

--- file.py
+++ file.py
@@ -1,5 +1,4 @@
 import requests
-import re
 from bs4 import BeautifulSoup
 
 
@@ -11,14 +10,13 @@ def get_content_str(url: str):
                         ).content.decode()
 
 
-def get_top_answer_questions_urls(topic_id: str = '19561847', pages_n: int = 3):
-    return set( ['http://www.zhihu.com' + url['href']
-            for k in range(1, pages_n + 1)
-            for url in BeautifulSoup(
+def get_top_answer_questions_urls(topic_id: str = '19591186', pages_n: int = 3):
+    return set(['http://www.zhihu.com' + url['href']
+                for k in range(1, pages_n + 1)
+                for url in BeautifulSoup(
             get_content_str('https://www.zhihu.com/topic/' + topic_id + '/top-answers?page=' + str(k))
             , 'html5lib').find_all(class_='question_link')])
 
-top_answers_questions_urls = get_top_answer_questions_urls()
 
 def get_photos(page_url: str):
     content_str = get_content_str(page_url)
@@ -35,6 +33,28 @@ def get_photos(page_url: str):
         with open(cur_dir + '/' + str(k) + '.jpg', 'wb') as photo_file:
             photo_file.write(requests.get(photo_url).content)
 
+def get_search_result(url:str):
+    page = get_content_str(url)
+    soup = BeautifulSoup(page,'html5lib')
+    question_urls = [ 'http://www.zhihu.com' +card['href'] for card in soup.find_all(class_='js-title-link') if len(card['href'])==18]
+    js_request = 'https://www.zhihu.com/r/search?q='+url[44:]+'&correction=1&type=content&offset=10'
+    import json
+    js_response_urls = [ BeautifulSoup(li,'html5lib').find_all(class_='js-title-link')[0]['href']  for li in  json.loads(get_content_str(js_request))['htmls'] ]
+    while len(js_response_urls):
+        for response_url in js_response_urls:
+                if len(response_url)==18:
+                    question_urls.append( 'http://www.zhihu.com'+ response_url)
+        js_response_urls = [BeautifulSoup(card,'html5lib').find_all(class_='js-title-link')[0]['href'] for card in
+                            json.loads(get_content_str(js_request))['htmls']]
+    from pandas import DataFrame
+    questions_tbl = DataFrame({'url':question_urls})
+    questions_tbl.to_csv('result_of_search_'+url[44:]+'.csv')
+    return question_urls
 
-for page_url in top_answers_questions_urls:
-    get_photos(page_url)
+search_result = get_search_result(
+    'https://www.zhihu.com/search?type=content&q=%E7%A7%81%E6%88%BF%E7%85%A7'
+)
+
+for url in search_result:
+    get_photos(url)
+
